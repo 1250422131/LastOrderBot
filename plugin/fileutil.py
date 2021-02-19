27d34944c132eed98dc13ad2cf5e49b967c0b1f2
萌新杰少 <52126790+1250422131@users.noreply.github.com>
2021-02-19 17:53:59 +0800

    新增使用系统判断
    纠正了Linux使用文件读写出错问题

--- plugin/fileutil.py
+++ plugin/fileutil.py
@@ -1,39 +1,50 @@
 import os
+import sys
 class FileUtil:
 
     # 全局储存目录
     storage_path = os.path.join(os.getcwd(), "data")
+    system_name = sys.platform
+    #默认win环境
+    catalog_symbol = "\\"
+
+    def __init__(self):
+        if "win" in self.system_name :
+            self.catalog_symbol = "\\"
+        else:
+            self.catalog_symbol = "/"
+
 
     def write(self, file_path,file_name , msg):
         # 去除首位空格
-        path = self.storage_path + "\\" + str(file_path)
+        path = self.storage_path + self.catalog_symbol + str(file_path)
         path = path.strip()
-        # 去除尾部 \ 符号
-        path= path.rstrip("\\")
+        # 去除尾部 目录分割 符号
+        path= path.rstrip(self.catalog_symbol)
         isExists = os.path.exists(path)
         if isExists:
-            file = open(path + "\\" + file_name, "w+", encoding="utf-8")
+            file = open(path + self.catalog_symbol + file_name, "w+", encoding="utf-8")
             file.write(msg)
             file.close
         else:
             #创建多文件夹 创建自定义目录则 -> mkdir
             os.makedirs(path)
-            file = open(path + "\\" + file_name, "w+", encoding="utf-8")
+            file = open(path + self.catalog_symbol + file_name, "w+", encoding="utf-8")
             file.write(msg)
             file.close
 
 
     def read(self, file_path,file_name):
-        path = self.storage_path + "\\" + str(file_path)
+        path = self.storage_path + self.catalog_symbol + str(file_path)
         path = path.strip()
-        # 去除尾部 \ 符号
-        path= path.rstrip("\\")
+        # 去除尾部 目录分割 符号
+        path= path.rstrip(self.catalog_symbol)
         #判断文件夹是否存在
         isExists = os.path.exists(path)
         if isExists:
             #判断文件是否存在
-            if os.path.exists(path + "\\" + file_name):
-                file = open(path + "\\" + file_name, "r", encoding="utf-8")
+            if os.path.exists(path + self.catalog_symbol + file_name):
+                file = open(path + self.catalog_symbol + file_name, "r", encoding="utf-8")
                 msg = file.read()
                 print(msg)
                 file.close
